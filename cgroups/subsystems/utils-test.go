d4c0a9f01dcec60adc0c290817f1f4e0587fe22d
nicktming <2279563278@qq.com>
2019-03-31 18:57:37 +0800

    update util test

--- cgroups/subsystems/utils-test.go
+++ cgroups/subsystems/utils-test.go
@@ -8,9 +8,17 @@ import (
 	"time"
 )
 
+func Test000(t *testing.T)  {
+	mountPath := FindCgroupMountPoint("memory")
+	log.Printf("mountPath:%s\n", mountPath)
+}
+
 func Test001(t *testing.T)  {
-	//paths := FindAbsolutePath("memory")
-	//log.Printf("paths:%s\n", paths)
+	absolutePath := FindAbsolutePath("memory")
+	log.Printf("absolutePath:%s\n", absolutePath)
+}
+
+func Test002(t *testing.T)  {
 	Set("10M")
 	pid := os.Getpid()
 	log.Printf("current pid : %s\n", strconv.Itoa(pid))
