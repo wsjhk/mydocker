85039510b78a52656fe39c9ffd054fe2e6b0eeb8
nicktming <2279563278@qq.com>
2019-04-13 17:49:04 +0800

    add dev-5.4

--- command/exec.go
+++ command/exec.go
@@ -1,5 +1,22 @@
 package command
 
-func Exec(containerName, command string) {
+import (
+	"fmt"
+	"os"
+)
 
+var (
+	MYDOCKER_PID = "mydocker_pid"
+	MYDOCKER_COMMAND = "mydocker_cmd"
+)
+
+func Exec(containerName, command string) {
+	containerInfo, err := GetContainerInfo(containerName)
+	if err != nil {
+		fmt.Errorf("GetContainerInfo error:%v\n", err)
+		return
+	}
+	pid := containerInfo.Pid
+	os.Setenv(MYDOCKER_PID, pid)
+	os.Setenv(MYDOCKER_COMMAND, command)
 }
