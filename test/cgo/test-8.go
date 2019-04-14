ce13dbd469c32da68275b67fa1ab2e21f2f8012c
nicktming <2279563278@qq.com>
2019-04-14 09:31:03 +0800

    add test-8.go

--- test/cgo/test-8.go
+++ test/cgo/test-8.go
@@ -5,10 +5,6 @@ package main
 __attribute__((constructor)) void before_main() {
    printf("before main\n");
 }
-
-__attribute__((destructor)) void after_main() {
-   printf("after main\n");
-}
 */
 import "C"
 
