3887fe1ef50337ff3a41a85a38ec5c77bbe23152
nicktming <2279563278@qq.com>
2019-04-18 22:11:11 +0800

    add command/run

--- command/commit.go
+++ command/commit.go
@@ -1,14 +1,22 @@
 package command
 
 import (
+	"fmt"
 	"log"
 	"os/exec"
 )
 
-func Commit(imageName string) {
-	mntPath := DEFAULTPATH + "/mnt"
-	imageTar := DEFAULTPATH + "/" + imageName + ".tar"
-	log.Printf("imageTar:%s\n", imageTar)
+func Commit(containerName, imageName string) {
+	//mntPath := DEFAULTPATH + "/mnt"
+	//imageTar := DEFAULTPATH + "/" + imageName + ".tar"
+	containerInfo, err := GetContainerInfo(containerName)
+	if err != nil {
+		fmt.Errorf("GetContainerInfo error:%v\n", err)
+		return
+	}
+	mntPath  := containerInfo.RootPath + "/mnt/" + containerName
+	imageTar := containerInfo.RootPath + "/" + imageName + ".tar"
+	log.Printf("mntPath:%s, imageTar:%s\n", mntPath, imageTar)
 	if _, err := exec.Command("tar", "-czf", imageTar, "-C", mntPath, ".").CombinedOutput(); err != nil {
 		log.Printf("Error: tar -czf %s -C %s .; err:%v\n", imageTar, mntPath, err)
 	}
