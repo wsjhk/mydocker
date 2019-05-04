c94c7ac02bf41ec65af6420ff024625c666dfde2
nicktming <2279563278@qq.com>
2019-05-04 22:08:16 +0800

    add api_test

--- network/api_test.go
+++ network/api_test.go
@@ -7,8 +7,11 @@ import (
 	"strings"
 	"testing"
 	"github.com/vishvananda/netlink"
+	"time"
 )
 
+
+// 类似于 ip link add name testbridge type bridge
 func TestNet001(t *testing.T) {
 	bridgeName := "testbridge"
 	_, err := net.InterfaceByName(bridgeName)
@@ -23,4 +26,31 @@ func TestNet001(t *testing.T) {
 	if err := netlink.LinkAdd(br); err != nil {
 		fmt.Errorf("Bridge creation failed for bridge %s: %v", bridgeName, err)
 	}
+}
+
+func TestNet002(t *testing.T) {
+	name := "testbridge"
+	rawIP := "192.168.0.0"
+	retries := 2
+	var iface netlink.Link
+	var err error
+	for i := 0; i < retries; i++ {
+		iface, err = netlink.LinkByName(name)
+		if err == nil {
+			break
+		}
+		log.Printf("error retrieving new bridge netlink link [ %s ]... retrying", name)
+		time.Sleep(2 * time.Second)
+	}
+	if err != nil {
+		fmt.Errorf("Abandoning retrieving the new bridge link from netlink, Run [ ip link ] to troubleshoot the error: %v", err)
+	}
+	ipNet, err := netlink.ParseIPNet(rawIP)
+	if err != nil {
+		log.Printf("ParseIPNet error:%v\n", err)
+	}
+	log.Printf("ipNet:%v\n", ipNet)
+	addr := &netlink.Addr{ipNet, "", 0, 0, nil}
+	err = netlink.AddrAdd(iface, addr)
+	log.Printf("AddrAdd error:%v\n", err)
 }
