6a21a932f6138454c6d6f179c8e4ab19355f9649
nicktming <2279563278@qq.com>
2019-05-04 21:51:32 +0800

    add api_test

--- network/api_test.go
+++ network/api_test.go
@@ -1,10 +1,12 @@
 package network
 
 import (
+	"fmt"
 	"log"
 	"net"
 	"strings"
 	"testing"
+	"github.com/vishvananda/netlink"
 )
 
 func TestNet001(t *testing.T) {
@@ -12,5 +14,13 @@ func TestNet001(t *testing.T) {
 	_, err := net.InterfaceByName(bridgeName)
 	if err == nil || !strings.Contains(err.Error(), "no such network interface") {
 		log.Printf("error:%v\n", err)
+	}
+	// create *netlink.Bridge object
+	la := netlink.NewLinkAttrs()
+	la.Name = bridgeName
+
+	br := &netlink.Bridge{la}
+	if err := netlink.LinkAdd(br); err != nil {
+		fmt.Errorf("Bridge creation failed for bridge %s: %v", bridgeName, err)
 	}
 }
