8a0e2d0e03faf75481d04230a10cad6e88b62cf0
nicktming <2279563278@qq.com>
2019-05-05 20:39:05 +0800

    add api_test

--- network/api_test.go
+++ network/api_test.go
@@ -11,7 +11,7 @@ import (
 )
 
 
-// 类似于 ip link add name testbridge type bridge
+// 等于 ip link add name testbridge type bridge
 func TestNet001(t *testing.T) {
 	bridgeName := "testbridge"
 	_, err := net.InterfaceByName(bridgeName)
@@ -23,6 +23,7 @@ func TestNet001(t *testing.T) {
 	la.Name = bridgeName
 
 	br := &netlink.Bridge{la}
+	// 等于 ip link add name testbridge type bridge
 	if err := netlink.LinkAdd(br); err != nil {
 		fmt.Errorf("Bridge creation failed for bridge %s: %v", bridgeName, err)
 	}
@@ -35,6 +36,7 @@ func TestNet002(t *testing.T) {
 	var iface netlink.Link
 	var err error
 	for i := 0; i < retries; i++ {
+		// 根据名字找到设备
 		iface, err = netlink.LinkByName(name)
 		if err == nil {
 			break
@@ -45,15 +47,20 @@ func TestNet002(t *testing.T) {
 	if err != nil {
 		fmt.Errorf("Abandoning retrieving the new bridge link from netlink, Run [ ip link ] to troubleshoot the error: %v", err)
 	}
+	// 将原始ip转换成*net.IPNet类型
 	ipNet, err := netlink.ParseIPNet(rawIP)
 	if err != nil {
 		log.Printf("ParseIPNet error:%v\n", err)
 	}
+
 	log.Printf("ipNet:%v\n", ipNet)
 	addr := &netlink.Addr{ipNet, "", 0, 0, nil}
+
+	// 等于 ip addr add 192.168.0.1/24 dev testbridge
 	err = netlink.AddrAdd(iface, addr)
 	log.Printf("AddrAdd error:%v\n", err)
 
+	// 等于 ip link set testbridge up
 	if err := netlink.LinkSetUp(iface); err != nil {
 		fmt.Errorf("Error enabling interface for %s: %v", name, err)
 	}
