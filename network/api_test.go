c2f3814c23b9d06c821badbf76c2b3b99e858372
nicktming <2279563278@qq.com>
2019-05-05 21:55:56 +0800

    add dev-6.1:dev-6.1

--- network/api_test.go
+++ network/api_test.go
@@ -2,11 +2,15 @@ package network
 
 import (
 	"fmt"
+	"github.com/Sirupsen/logrus"
+	"github.com/vishvananda/netlink"
+	"github.com/vishvananda/netns"
 	"log"
 	"net"
+	"os"
+	"runtime"
 	"strings"
 	"testing"
-	"github.com/vishvananda/netlink"
 	"time"
 )
 
@@ -119,4 +123,115 @@ func deleteDevice(name string)  {
 		return
 	}
 	log.Printf("Delete Device %s\n", name)
+}
+
+func TestNet006(t *testing.T) {
+	PeerName := "cif-12345"
+	containerIP := "192.168.0.8/24"
+	gwIP := "192.168.0.1"
+	ConfigEndpointIpAddressAndRoute(PeerName, containerIP, gwIP)
+}
+
+
+func EnterContainerNetns(enLink *netlink.Link) func() {
+	f, err := os.OpenFile(fmt.Sprintf("/proc/%s/ns/net", "16694"), os.O_RDONLY, 0)
+	if err != nil {
+		logrus.Errorf("error get container net namespace, %v", err)
+	}
+
+	nsFD := f.Fd()
+	runtime.LockOSThread()
+
+	// 修改veth peer 另外一端移到容器的namespace中
+	if err = netlink.LinkSetNsFd(*enLink, int(nsFD)); err != nil {
+		logrus.Errorf("error set link netns , %v", err)
+	}
+
+	// 获取当前的网络namespace
+	origns, err := netns.Get()
+	if err != nil {
+		logrus.Errorf("error get current netns, %v", err)
+	}
+
+	// 设置当前进程到新的网络namespace，并在函数执行完成之后再恢复到之前的namespace
+	if err = netns.Set(netns.NsHandle(nsFD)); err != nil {
+		logrus.Errorf("error set netns, %v", err)
+	}
+	return func () {
+		netns.Set(origns)
+		origns.Close()
+		runtime.UnlockOSThread()
+		f.Close()
+	}
+}
+
+func ConfigEndpointIpAddressAndRoute(PeerName, containerIP, gwIP string) error {
+	peerLink, err := netlink.LinkByName(PeerName)
+	if err != nil {
+		return fmt.Errorf("fail config endpoint: %v", err)
+	}
+
+	defer EnterContainerNetns(&peerLink)()
+
+	if err = setInterfaceIP(PeerName, containerIP); err != nil {
+		return fmt.Errorf("set %s up error:%v", PeerName, err)
+	}
+
+	if err = setInterfaceUP(PeerName); err != nil {
+		return err
+	}
+
+	if err = setInterfaceUP("lo"); err != nil {
+		return err
+	}
+
+	_, cidr, _ := net.ParseCIDR("0.0.0.0/0")
+
+	defaultRoute := &netlink.Route{
+		LinkIndex: peerLink.Attrs().Index,
+		Gw: []byte(gwIP),
+		Dst: cidr,
+	}
+
+	if err = netlink.RouteAdd(defaultRoute); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func setInterfaceUP(interfaceName string) error {
+	iface, err := netlink.LinkByName(interfaceName)
+	if err != nil {
+		return fmt.Errorf("Error retrieving a link named [ %s ]: %v", iface.Attrs().Name, err)
+	}
+
+	if err := netlink.LinkSetUp(iface); err != nil {
+		return fmt.Errorf("Error enabling interface for %s: %v", interfaceName, err)
+	}
+	return nil
+}
+
+// Set the IP addr of a netlink interface
+func setInterfaceIP(name string, rawIP string) error {
+	retries := 2
+	var iface netlink.Link
+	var err error
+	for i := 0; i < retries; i++ {
+		iface, err = netlink.LinkByName(name)
+		if err == nil {
+			break
+		}
+		log.Printf("error retrieving new bridge netlink link [ %s ]... retrying", name)
+		time.Sleep(2 * time.Second)
+	}
+	if err != nil {
+		return fmt.Errorf("Abandoning retrieving the new bridge link from netlink, Run [ ip link ] to troubleshoot the error: %v", err)
+	}
+	ipNet, err := netlink.ParseIPNet(rawIP)
+	if err != nil {
+		return err
+	}
+	addr := &netlink.Addr{ipNet, "", 0, 0, nil}
+	return netlink.AddrAdd(iface, addr)
 }
